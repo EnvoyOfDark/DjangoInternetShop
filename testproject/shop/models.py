03d9aca1c9625fd0543d89a8eec47adbdd020c49
EnvoyOfDark <112541003+EnvoyOfDark@users.noreply.github.com>
2023-03-20 19:59:31 +0200

    Delete testproject directory

--- testproject/shop/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Shop(models.Model):
-    title = models.CharField(max_length=50)
-    description = models.TextField()
-    date = models.DateField()
-    price = models.FloatField()
-    amount = models.IntegerField()
